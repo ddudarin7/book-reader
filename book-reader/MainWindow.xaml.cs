a726b7a7c721eb49221350d3f1fd8082b0af50c1
Novak <novak.marin95@gmail.com>
2017-04-06 21:43:04 +0200

    Dodat import

--- book-reader/MainWindow.xaml.cs
+++ book-reader/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -33,6 +34,24 @@ namespace book_reader
         private void MainMenu_MouseLeave(object sender, MouseEventArgs e)
         {
             MainMenu.Visibility = Visibility.Hidden;
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Ovo radi");
+            System.Diagnostics.Debug.WriteLine("Ovo radi");
+            var fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.DefaultExt = ".txt"; // Default file extension
+            fileDialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+            var result = fileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                pageTxt.Text = fileDialog.FileName;
+
+                pageTxt.Text = File.ReadAllText(fileDialog.FileName);
+
+            }
         }
     }
 }
